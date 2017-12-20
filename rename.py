178fe880d2352958ad05b4da4695279e6ba1c89c
Andy Mao <dmao1@mail.sfsu.edu>
2017-12-20 00:41:49 -0800

    di

--- rename.py
+++ rename.py
@@ -2,7 +2,7 @@
 import os
 from datetime import timedelta, datetime
 from time import sleep
-
+import re
 
 def date_to_folder(date):
     time_str = date.strftime("%Y-%m-%d").split("-")
@@ -13,14 +13,16 @@ def rename(path, time_str):#time_str[0]: year, time_str[1]:month, time_str[2]:da
     if os.path.exists(path):
         i=1
         filelist=os.listdir(path)#该文件夹下所有的文件（包括文件夹）
+        if len(filelist) == 0:
+            return
         for files in sorted(filelist):#遍历所有文件
             Olddir=os.path.join(path,files)#原来的文件路径
             if os.path.isdir(Olddir):#如果是文件夹则跳过
                 continue
             filename=os.path.splitext(files)[0]#文件名
             filetype=os.path.splitext(files)[1]#文件扩展名
-            if files == time_str[2]+"00.flv" or files == time_str[2]+"99.flv":
-                continue                
+            if files == time_str[2]+"00.flv" or files == time_str[2]+"99.flv" or (re.search('.jpg?',files) is not None):
+                continue
             elif files == "00.flv" or files == "99.flv":
                 Newdir=os.path.join(path,time_str[2]+filename+filetype)
                 os.rename(Olddir,Newdir)
@@ -34,10 +36,10 @@ def rename(path, time_str):#time_str[0]: year, time_str[1]:month, time_str[2]:da
             if os.path.exists(Newdir):
                 pass
             else:
-                os.rename(Olddir,Newdir)#重命名		
+                os.rename(Olddir,Newdir)#重命名
     else:
         input("重命名地址错误")
-        exit()	
+        exit()
 
 
 
