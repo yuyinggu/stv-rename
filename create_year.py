2617176be23c6f2dea093506d6f42af5ef346e98
yuyinggu <34705666+yuyinggu@users.noreply.github.com>
2018-03-20 11:49:53 -0700

    helen

--- create_year.py
+++ create_year.py
@@ -10,8 +10,8 @@ def check_next_month(mon=1):
     next_month = (datetime.now() + timedelta(days=30*mon)).strftime("%Y-%m").split("-")
     print(next_month)
     try:
-        #test_path = "\\\\vdisk.chineseradio.local\\VideoWork\\OtherVideos\\STPlayer\Source\\"+ next_month[0] + "\\"+ next_month[1]
-        test_path = "C:\\Users\\Andy\\Downloads\\"+ next_month[0] + "\\"+ next_month[1] ###修改这里
+        test_path = "\\\\vdisk.chineseradio.local\\VideoWork\\OtherVideos\\STPlayer\Source\\"+ next_month[0] + "\\"+ next_month[1]
+        #test_path = "C:\\Users\\Andy\\Downloads\\"+ next_month[0] + "\\"+ next_month[1] ###修改这里
     except:
         os.system("pause")
         error = input("地址错误")
@@ -24,8 +24,8 @@ def check_next_month(mon=1):
         while cur_mon == start_mon:
             fold_str = date_to_folder(start_date)
             for i in range(1,9):
-                #dir_path = "\\\\vdisk.chineseradio.local\\VideoWork\\OtherVideos\\STPlayer\\Source\\"+ fold_str +"\\segment_0"+ str(i)
-                dir_path = "C:\\Users\\Andy\\Downloads\\" + fold_str +"\\segment_0"+ str(i)
+                dir_path = "\\\\vdisk.chineseradio.local\\VideoWork\\OtherVideos\\STPlayer\\Source\\"+ fold_str +"\\segment_0"+ str(i)
+                #dir_path = "C:\\Users\\Andy\\Downloads\\" + fold_str +"\\segment_0"+ str(i)
                 os.makedirs(dir_path)
             start_date = start_date + timedelta(days=1)
             cur_mon = int(start_date.strftime("%m"))
